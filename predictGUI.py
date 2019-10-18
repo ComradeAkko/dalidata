a0258bd69808f28940b71340562b93842edae760
ComradeAkko <jirojpn2412@gmail.com>
2019-10-18 23:21:27 +0100

    finished general layout of app

--- predictGUI.py
+++ predictGUI.py
@@ -28,14 +28,28 @@ class App(QDialog):
         self.createErrorBox()
         self.createScrollBox(attriList)
         self.createCalcBox()
+        self.createResultsLabel()
+        self.createModelLabel()
+        self.createRMSEbox()
+        self.createErrorTwo()
+        self.createResuScroll()
+        self.createPredictBox()
+        self.createOutcomePred()
 
-
+        # layout all the boxes
         mainLayout = QGridLayout()
         mainLayout.addLayout(self.outcomeBox, 0, 0)
         mainLayout.addLayout(self.attriLabelBox, 1, 0)
         mainLayout.addLayout(self.errorBox, 2, 0)
         mainLayout.addLayout(self.scrollBox, 3, 0)
         mainLayout.addLayout(self.calcBox, 4, 0)
+        mainLayout.addLayout(self.resBox, 0, 1)
+        mainLayout.addLayout(self.modelBox, 1, 1)
+        mainLayout.addLayout(self.rmseBox, 2, 1)
+        mainLayout.addLayout(self.errorTwo, 3, 1)
+        mainLayout.addLayout(self.resuScroll, 4, 1)
+        mainLayout.addLayout(self.predBox, 5, 1)
+        mainLayout.addLayout(self.outcomePred, 6, 1)
 
         self.setLayout(mainLayout)
 
@@ -120,14 +134,75 @@ class App(QDialog):
         self.calcBox = QHBoxLayout()
 
         # create calculate button
-        calcButton = QPushButton('Model Regression', self)
-        calcButton.setToolTip("Calculates regression models")
+        modelButton = QPushButton('Model Regression', self)
+        modelButton.setToolTip("Calculates regression models")
         # calcButton.clicked.connect(self, ---)
 
-        self.calcBox.addWidget(calcButton)
+        self.calcBox.addWidget(modelButton)
 
-    # create 
+    # create box that contains the results label
+    def createResultsLabel(self):
+        self.resBox = QHBoxLayout()
+        
+        # create label
+        resLabel = QLabel("Results:")
 
+        self.resBox.addWidget(resLabel)
+
+    # create box that contains the model label
+    def createModelLabel(self):
+        self.modelBox = QHBoxLayout()
+
+        # create label
+        modelLabel = QLabel("Model used:")
+        self.modelBox.addWidget(modelLabel)
+
+    # create box that contains rmse label
+    def createRMSEbox(self):
+        self.rmseBox = QHBoxLayout()
+
+        rmseLabel = QLabel("RMSE:")
+        self.rmseBox.addWidget(rmseLabel)
+
+    # create box that contains error label 2
+    def createErrorTwo(self):
+        self.errorTwo = QHBoxLayout()
+        
+        errorTwoLabel = QLabel(" ")
+        self.errorTwo.addWidget(errorTwoLabel)
+
+    # creates a second scroll box for attribute boxes
+    def createResuScroll(self):
+        self.resuScroll = QVBoxLayout()
+        
+        # create box layout within the box
+        scrollInbox = QVBoxLayout()
+
+        # create scroll area widget
+        scrollSpace = QScrollArea()
+        scrollSpace.setLayout(scrollInbox)
+
+        # set box layout
+        self.resuScroll.addWidget(scrollSpace)
+
+    # creates a box for predict button
+    def createPredictBox(self):
+        self.predBox = QHBoxLayout()
+
+        # create predict button
+        predButton = QPushButton('Predict', self)
+        predButton.setToolTip("Predicts outcome based on model")
+        # predButton.clicked.connect(self, ---)
+
+        self.predBox.addWidget(predButton)
+
+    # create box that contains outcome prediction
+    def createOutcomePred(self):
+        self.outcomePred = QHBoxLayout()
+
+        outcomeLabel = QLabel('Outcome prediction:')
+
+        self.outcomePred.addWidget(outcomeLabel)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
